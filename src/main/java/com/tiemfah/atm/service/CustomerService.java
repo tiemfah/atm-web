b0dcbce80d3c67b47ab7acfbfebc273147cb5e24
tiemfah <sivanat.sub@gmail.com>
2020-09-22 14:18:23 +0700

    switch to use mysql db

--- src/main/java/com/tiemfah/atm/service/CustomerService.java
+++ src/main/java/com/tiemfah/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package com.tiemfah.atm.service;
 
+import com.tiemfah.atm.data.CustomerRepository;
 import org.mindrot.jbcrypt.BCrypt;
 import com.tiemfah.atm.model.Customer;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -10,52 +12,43 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+    private CustomerRepository repository;
 
-    private List<Customer> customerList;
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     private String hash(String pin) {
         String salt = BCrypt.gensalt(12);
         return BCrypt.hashpw(pin, salt);
     }
 
-    @PostConstruct
-    public void postConstruct() {
-        this.customerList = new ArrayList<>();
+    public Customer checkPin(Customer inputCustomer) {
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        return null;
     }
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
     }
-
-    public Customer checkPin(Customer inputCustomer) {
-        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
-        Customer storedCustomer = findCustomer(inputCustomer.getId());
-
-        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
-        if (storedCustomer != null) {
-            String hashPin = storedCustomer.getPin();
-
-            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
-                return storedCustomer;
-        }
-        // 3. ถ้าไม่ตรง ต้องคืนค่า null
-        return null;
-    }
-
-
 }
 
